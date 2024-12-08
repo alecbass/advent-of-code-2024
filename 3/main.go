c61be099a04c16d2b2930ab6c7014c98bf707e80
alecbass <alec.bassingthwaighte@gmail.com>
2024-12-08 15:42:28 +1100

    Working day three part two :)

--- 3/main.go
+++ 3/main.go
@@ -9,7 +9,15 @@ import (
 
 const MUL_REGEX = `mul\((?<first>\d+),(?<second>\d+)\)`
 const DO_REGEX = `do\(\)`
-const DONT_REGEX = `dont'\(\)`
+const DO_OR_DONT_REGEX = `(do\(\))|(don't\(\))`
+
+// A mul() call in the file, plus the leftmost index of where it appears
+type Multiplication struct {
+	// The first multiplication value
+	first int
+	// The second multiplication value
+	second int
+}
 
 func readFile(fileName string) string {
 	contents, err := os.ReadFile(fileName)
@@ -21,42 +29,31 @@ func readFile(fileName string) string {
 	return string(contents)
 }
 
-// A mul() call in the file, plus the leftmost index of where it appears
-type Multiplication struct {
-	// The index of the contents in which this multiplication appears
-	index int
-	// The first multiplication value
-	first int
-	// The second multiplication value
-	second int
-}
-
-func parseSubMatch(subMatch []int, contents string) Multiplication {
-	first_index_start := subMatch[2]
-	first_index_end := subMatch[3]
-	second_index_start := subMatch[4]
-	second_index_end := subMatch[5]
+func parseSubMatch(sub_match []int, contents string) Multiplication {
+	// Where this multiplication appears in the file
+	first_index_start := sub_match[2]
+	first_index_end := sub_match[3]
+	second_index_start := sub_match[4]
+	second_index_end := sub_match[5]
 
 	first_mul := contents[first_index_start:first_index_end]
 	second_mul := contents[second_index_start:second_index_end]
-	fmt.Println(first_mul, second_mul)
 
 	// The subMatch is guaranteed to return a valid integer due to the regex
 	first, err := strconv.Atoi(first_mul)
 
 	// Terrible bad logic, I am shocking at Go
 	if err != nil {
-		return Multiplication{first: 0, second: 0}
+		panic("Invalid first number")
 	}
 
 	second, err := strconv.Atoi(second_mul)
 
 	if err != nil {
-		return Multiplication{first: 0, second: 0}
+		panic("Invalid second number")
 	}
 
 	return Multiplication{
-		index:  0,
 		first:  first,
 		second: second,
 	}
@@ -69,28 +66,60 @@ func main() {
 		panic("Usage: <executable> <input_file_name>")
 	}
 
-	regex := regexp.MustCompile(MUL_REGEX)
+	mul_regex := regexp.MustCompile(MUL_REGEX)
 	do_regex := regexp.MustCompile(DO_REGEX)
-	dont_regex := regexp.MustCompile(DONT_REGEX)
 
-	fileName := args[1]
-	contents := readFile(fileName)
+	file_name := args[1]
+	contents := readFile(file_name)
 
-	matches := regex.FindAllStringSubmatchIndex(contents, -1)
-	doMatches := do_regex.FindAllStringIndex(contents, -1)
-	dontMatches := dont_regex.FindAllStringIndex(contents, -1)
+	do_matches := do_regex.FindAllStringIndex(contents, -1)
 
-	total := 0
+	// Find all index ranges where do() has been called, and dont() has not
+	valid_ranges := [][]int{}
 
-	for _, subMatch := range matches {
-		// Find all multiplication values
-		fmt.Println(subMatch)
-		multiplication := parseSubMatch(subMatch, contents)
-		total += multiplication.first * multiplication.second
+	terminator_regex := regexp.MustCompile(DO_OR_DONT_REGEX)
+
+	for index, do_match := range do_matches {
+		do_start_index := do_match[0]
+
+		is_first := index == 0
+
+		if is_first {
+			// Since the list starts enabled, prepend with a valid range from the start do the first do() or don't() call
+			valid_ranges = append(valid_ranges, []int{0, do_start_index})
+		}
+
+		// Check this substring, ignoring the beginning "do()"
+		contents_slice := contents[do_start_index:]
+		terminator := terminator_regex.FindStringIndex(contents_slice[4:])
+
+		// Read to the end of the file by default
+		terminator_index := len(contents_slice) - 1
+
+		if len(terminator) > 1 {
+			// Not at the end of the file, i.e. we found a do() or don't() call
+			terminator_index = terminator[1]
+		}
+
+		valid_ranges = append(valid_ranges, []int{do_start_index + 4, do_start_index + terminator_index})
 	}
 
-	fmt.Println(total)
-	fmt.Println(doMatches)
-	fmt.Println(dontMatches)
+	// The running multiplication total
+	total := 0
 
+	for _, r := range valid_ranges {
+		start := r[0]
+		end := r[1]
+
+		content_slice := contents[start:end]
+		range_mul_matches := mul_regex.FindAllStringSubmatchIndex(content_slice, -1)
+
+		for _, mul_match := range range_mul_matches {
+			// Find all mul() calls within this given range
+			multiplication := parseSubMatch(mul_match, content_slice)
+			total += multiplication.first * multiplication.second
+		}
+	}
+
+	fmt.Printf("Total: %d\n", total)
 }
